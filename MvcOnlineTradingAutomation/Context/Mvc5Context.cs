c0733d7ec05328469496bdcffd24943423970058
aliyilmaz020 <ali.yilmaz.199920@icloud.com>
2025-02-12 18:28:16 +0300

    migration

---
+++ MvcOnlineTradingAutomation/Context/Mvc5Context.cs
@@ -0,0 +1,23 @@
+﻿using MvcOnlineTradingAutomation.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTradingAutomation.Context
+{
+    public class Mvc5Context : DbContext
+    {
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<InvoiceDocument> InvoicesDocument { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<SaleOperation> SalesOperations { get; set; }
+    }
+}
