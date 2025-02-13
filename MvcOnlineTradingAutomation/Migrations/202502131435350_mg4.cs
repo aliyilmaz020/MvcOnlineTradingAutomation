f32339d44ce146aa4fe38c5fd1f4c6815c44e9fd
aliyilmaz020 <ali.yilmaz.199920@icloud.com>
2025-02-13 17:39:24 +0300

    default value mig

---
+++ MvcOnlineTradingAutomation/Migrations/202502131435350_mg4.cs
@@ -0,0 +1,18 @@
+﻿namespace MvcOnlineTradingAutomation.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mg4 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Departments", "Status", c => c.Boolean(nullable: false,defaultValue:true));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Departments", "Status");
+        }
+    }
+}
