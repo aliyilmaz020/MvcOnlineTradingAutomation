afafe06589c99f037af7f8e3fc70078ae72e83bd
aliyilmaz020 <ali.yilmaz.199920@icloud.com>
2025-02-13 12:50:18 +0300

    Category

---
+++ MvcOnlineTradingAutomation/Controllers/CategoryController.cs
@@ -0,0 +1,56 @@
+﻿using MvcOnlineTradingAutomation.Context;
+using MvcOnlineTradingAutomation.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTradingAutomation.Controllers
+{
+    public class CategoryController : Controller
+    {
+        Mvc5Context db = new Mvc5Context();
+
+        // GET: Category
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var categories = db.Categories.ToList();
+            return View(categories);
+        }
+        [HttpGet]
+        public ActionResult CreateCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateCategory(Category c)
+        {
+            db.Categories.Add(c);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult RemoveCategory(int id)
+        {
+            var value = db.Categories.Find(id);
+            db.Categories.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult EditCategory(int id)
+        {
+            var category = db.Categories.Find(id);
+            return View(category);
+        }
+        [HttpPost]
+        public ActionResult EditCategory(Category category)
+        {
+            var value = db.Categories.Find(category.CategoryId);
+            value.CategoryName = category.CategoryName;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
