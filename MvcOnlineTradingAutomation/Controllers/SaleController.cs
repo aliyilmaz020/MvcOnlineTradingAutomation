ae701e330e82dbe7200f470ed9709d5e78e5ae09
aliyilmaz020 <ali.yilmaz.199920@icloud.com>
2025-02-15 00:14:21 +0300

    input replace

---
+++ MvcOnlineTradingAutomation/Controllers/SaleController.cs
@@ -0,0 +1,53 @@
+﻿using MvcOnlineTradingAutomation.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTradingAutomation.Models.Entities;
+
+namespace MvcOnlineTradingAutomation.Controllers
+{
+    public class SaleController : Controller
+    {
+        Mvc5Context db = new Mvc5Context();
+        // GET: Sale
+        public ActionResult Index()
+        {
+            var sales = db.SalesOperations.ToList();
+            return View(sales);
+        }
+        [HttpGet]
+        public ActionResult CreateSale()
+        {
+            List<SelectListItem> product = db.Products.Where(x=>x.ProductStatus==true).Select(x => new SelectListItem
+            {
+                Value = x.ProductId.ToString(),
+                Text = x.ProductName
+            }).ToList();
+            ViewBag.Product = product;
+            List<SelectListItem> customer = db.Customers.Where(x=>x.Status==true).Select(x => new SelectListItem
+            {
+                Value = x.CustomerId.ToString(),
+                Text = x.CustomerName + " " + x.CustomerSurname
+            }).ToList();
+            ViewBag.Customer = customer;
+            List<SelectListItem> employee = db.Employees.Where(x=>x.Status==true).Select(x => new SelectListItem
+            {
+                Value = x.EmployeeId.ToString(),
+                Text = x.EmployeeName + " " + x.EmployeeSurname
+            }).ToList();
+            ViewBag.Employee = employee;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateSale(SaleOperation sale)
+        {
+            sale.Total = sale.Price * sale.Quantity;
+            sale.Date = DateTime.Now;
+            db.SalesOperations.Add(sale);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
