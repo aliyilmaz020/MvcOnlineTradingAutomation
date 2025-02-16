48586efb43b32e67840c8c16b5bb99146317d2af
aliyilmaz020 <ali.yilmaz.199920@icloud.com>
2025-02-16 16:57:11 +0300

    IEnumerable

---
+++ MvcOnlineTradingAutomation/Models/Entities/Enums.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTradingAutomation.Models.Entities
+{
+    public class Enums
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<Detail> Details { get; set; }
+    }
+}
