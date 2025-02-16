ea5a3187227446993f0c8747e58784c12c34f8dc
aliyilmaz020 <ali.yilmaz.199920@icloud.com>
2025-02-16 15:30:06 +0300

    Gallery

---
+++ MvcOnlineTradingAutomation/Controllers/GalleryController.cs
@@ -0,0 +1,20 @@
+﻿using MvcOnlineTradingAutomation.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTradingAutomation.Controllers
+{
+    public class GalleryController : Controller
+    {
+        Mvc5Context db = new Mvc5Context();
+        // GET: Gallery
+        public ActionResult Index()
+        {
+            var values = db.Products.Where(x=>x.ProductStatus==true).ToList();
+            return View(values);
+        }
+    }
+}
