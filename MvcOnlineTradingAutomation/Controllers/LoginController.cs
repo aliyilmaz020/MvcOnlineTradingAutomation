ef51267ef81b8ead3e6c8c8a78dffa26d79284d5
aliyilmaz020 <ali.yilmaz.199920@icloud.com>
2025-02-17 23:58:53 +0300

    Sign in customer

--- MvcOnlineTradingAutomation/Controllers/LoginController.cs
+++ MvcOnlineTradingAutomation/Controllers/LoginController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MvcOnlineTradingAutomation.Context;
+using MvcOnlineTradingAutomation.Models.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,12 +11,22 @@ namespace MvcOnlineTradingAutomation.Controllers
     public class LoginController : Controller
     {
         // GET: Login
+        Mvc5Context db = new Mvc5Context();
         public ActionResult Index()
         {
             return View();
         }
+        [HttpGet]
         public PartialViewResult Partial1()
         {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult Partial1(Customer c)
+        {
+            c.Status = false;
+            db.Customers.Add(c);
+            db.SaveChanges();
             return PartialView();
         }
     }
