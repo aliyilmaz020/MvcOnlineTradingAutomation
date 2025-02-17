438ae2baf18eb94e510c719a373331d7b12d263c
aliyilmaz020 <ali.yilmaz.199920@icloud.com>
2025-02-18 01:17:39 +0300

    Customer Panel Session Data Move

--- MvcOnlineTradingAutomation/Controllers/CustomerPanelController.cs
+++ MvcOnlineTradingAutomation/Controllers/CustomerPanelController.cs
@@ -1,14 +1,32 @@
-﻿using System.Web.Mvc;
+﻿using MvcOnlineTradingAutomation.Context;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace MvcOnlineTradingAutomation.Controllers
 {
     public class CustomerPanelController : Controller
     {
+        Mvc5Context db = new Mvc5Context();
         [Authorize]
         // GET: CustomerPanel
         public ActionResult Index()
         {
+            string mail = (string)Session["CustomerMail"];
+            ViewBag.Mail = mail;
+
+            var values = db.Customers.FirstOrDefault(x=>x.CustomerMail == mail);
+            return View(values);
+        }
+        public ActionResult Index2()
+        {
             return View();
+        }
+        public ActionResult Orders()
+        {
+            string mail = (string)Session["CustomerMail"];
+            var id = db.Customers.Where(x=>x.CustomerMail==mail).Select(x=>x.CustomerId).FirstOrDefault();
+            var values = db.SalesOperations.Where(x=>x.CustomerId==id).ToList();
+            return View(values);
         }
     }
 }
