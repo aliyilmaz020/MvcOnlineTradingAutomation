fcebe7042efc44e81e2c124d4f3bd1982d2bd7a6
aliyilmaz020 <ali.yilmaz.199920@icloud.com>
2025-02-17 18:51:39 +0300

    todolist cards

--- MvcOnlineTradingAutomation/Controllers/ToDoListController.cs
+++ MvcOnlineTradingAutomation/Controllers/ToDoListController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcOnlineTradingAutomation.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,8 +10,13 @@ namespace MvcOnlineTradingAutomation.Controllers
     public class ToDoListController : Controller
     {
         // GET: ToDoList
+        Mvc5Context db = new Mvc5Context();
         public ActionResult Index()
         {
+            ViewBag.d1 = db.Employees.Where(x=>x.Status==true).Count().ToString();
+            ViewBag.d2 = db.Products.Where(x=>x.ProductStatus==true).Count().ToString();
+            ViewBag.d3 = db.Categories.Count().ToString();
+            ViewBag.d4 = db.Employees.Where(x=>x.Status==true).Select(x=>x.EmployeeCity).Distinct().Count().ToString();
             return View();
         }
     }
