6744cb84f7167bc2119309812a306dadc8886118
aliyilmaz020 <ali.yilmaz.199920@icloud.com>
2025-02-22 16:22:18 +0300

    Chart base controller

--- MvcOnlineTradingAutomation/Controllers/ChartController.cs
+++ MvcOnlineTradingAutomation/Controllers/ChartController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace MvcOnlineTradingAutomation.Controllers
@@ -12,6 +13,15 @@ namespace MvcOnlineTradingAutomation.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult Index2()
+        {
+            var chart = new Chart(width: 600, height: 600);
+            chart.AddTitle("Kategori-Ürün Stok Sayısı")
+                .AddLegend("Stok")
+                .AddSeries("Değerler", xValue: new[] { "Beyaz Eşya", "Küçük Ev Aleti", "Mobilya" },
+                yValues: new[] { 200, 100, 345 }).Write();
+            return File(chart.ToWebImage().GetBytes(), "image/jpeg");
         }
     }
 }
