6f594be781588623b8002ba4e1df602987f41966
aliyilmaz020 <ali.yilmaz.199920@icloud.com>
2025-02-22 16:43:17 +0300

    charts database

--- MvcOnlineTradingAutomation/Controllers/ChartController.cs
+++ MvcOnlineTradingAutomation/Controllers/ChartController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MvcOnlineTradingAutomation.Context;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +12,7 @@ namespace MvcOnlineTradingAutomation.Controllers
     public class ChartController : Controller
     {
         // GET: Graphic
+        Mvc5Context db = new Mvc5Context();
         public ActionResult Index()
         {
             return View();
@@ -20,8 +23,21 @@ namespace MvcOnlineTradingAutomation.Controllers
             chart.AddTitle("Kategori-Ürün Stok Sayısı")
                 .AddLegend("Stok")
                 .AddSeries("Değerler", xValue: new[] { "Beyaz Eşya", "Küçük Ev Aleti", "Mobilya" },
-                yValues: new[] { 200, 100, 345 }).Write();
+                yValues: new[] { 200, 100, 345 });
             return File(chart.ToWebImage().GetBytes(), "image/jpeg");
+        }
+        public ActionResult Index3()
+        {
+            ArrayList xvalue = new ArrayList();
+            ArrayList yvalue = new ArrayList();
+
+            var results = db.Products.Where(x => x.ProductStatus == true).ToList();
+            results.ForEach(x => xvalue.Add(x.ProductName));
+            results.ForEach(x => yvalue.Add(x.ProductStock));
+            var charts = new Chart(width: 1000, height: 800)
+                .AddTitle("Stoklar")
+                .AddSeries(chartType: "Pie", name: "Stok", xValue: xvalue, yValues: yvalue);
+            return File(charts.ToWebImage().GetBytes(), "image/jpeg");
         }
     }
 }
